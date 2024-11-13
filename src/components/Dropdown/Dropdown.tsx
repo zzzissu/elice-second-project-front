d37990967adcacf8455d72e153f3a814edb36014
yeon <yeonnnnn.k@gmail.com>
2024-11-13 16:45:29 +0900

    🐣feat: dropdown 기능 구현

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,14 +1,31 @@
+import useDropdown from "../../hooks/useDropdown.tsx";
+
 import { S } from "./Dropdown.style.ts";
 
-const Dropdown = () => {
+interface DropdownProps {
+  options: string[];
+  width?: string;
+  height?: string;
+}
+
+const Dropdown = ({ options }: DropdownProps) => {
+  const { isOpen, selectedItem, handleSelect, handleToggle } =
+    useDropdown(options);
+
   return (
-    <S.DropdownContainer>
-      최신순 <S.DropdownIcon />
-      <S.DropdownList>
-        <S.DropdownItem>최신순</S.DropdownItem>
-        <S.DropdownItem>오래된순</S.DropdownItem>
-        <S.DropdownItem>인기순</S.DropdownItem>
-      </S.DropdownList>
+    <S.DropdownContainer onClick={handleToggle}>
+      {selectedItem} <S.DropdownIcon />
+      {isOpen && (
+        <S.DropdownList>
+          {options.map((option, idx) => {
+            return (
+              <S.DropdownItem key={idx} onClick={() => handleSelect(option)}>
+                {option}
+              </S.DropdownItem>
+            );
+          })}
+        </S.DropdownList>
+      )}
     </S.DropdownContainer>
   );
 };
