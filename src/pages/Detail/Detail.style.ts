ac6d0815400e996d0431a2f116027c0ad0df5999
yeon <yeonnnnn.k@gmail.com>
2024-11-14 14:11:32 +0900

    🐣feat: detail page 상단 UI 구현

--- src/pages/Detail/Detail.style.ts
+++ src/pages/Detail/Detail.style.ts
@@ -1,17 +1,107 @@
 import styled from "styled-components";
 
+interface ProductImgProps {
+  imgUrl: string;
+}
+
 export const S = {
-  DetailWrap: styled.main``,
-  UpperWrap: styled.div``,
-  ProductImg: styled.div``,
-  ProductInfo: styled.div``,
-  ProductName: styled.div``,
-  ProductPrice: styled.div``,
-  SellerBox: styled.div``,
-  SellerIcon: styled.div``,
-  SellerNick: styled.span``,
-  DeliveryBox: styled.div``,
-  DeliveryIcon: styled.div``,
-  DeliveryPrice: styled.span``,
-  BtnWrap: styled.div``,
+  DetailWrap: styled.main`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    margin-top: 100px;
+    width: 1260px;
+
+    margin: 0 auto;
+  `,
+  Detail: styled.div`
+    margin-top: 60px;
+  `,
+  UpperWrap: styled.div`
+    display: flex;
+    gap: 60px;
+
+    margin-top: 40px;
+
+    width: 100%;
+    height: 408px;
+  `,
+  ProductImg: styled.div<ProductImgProps>`
+    background-image: url(${(props) => props.imgUrl});
+    background-size: cover;
+    background-position: center;
+    width: 456px;
+    height: 408px;
+
+    border-radius: 10px;
+  `,
+  ProductInfo: styled.div`
+    position: relative;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    width: 408px;
+  `,
+  EditBtn: styled.div`
+    position: absolute;
+    top: 8px;
+    right: 0;
+
+    background-image: url("/icons/edit.svg");
+    width: 20px;
+    height: 20px;
+
+    &:hover {
+      cursor: pointer;
+    }
+  `,
+  ProductName: styled.div`
+    font-size: 20px;
+    margin-bottom: 20px;
+  `,
+  ProductPrice: styled.div`
+    display: flex;
+    align-items: baseline;
+    gap: 6px;
+    margin-bottom: 10px;
+  `,
+  Bold: styled.span`
+    font-size: ${(props) => props.theme.font.title};
+    font-weight: 800;
+  `,
+  InfoBox: styled.div`
+    display: grid;
+    grid-template-columns: repeat(1, 1fr 14fr);
+    gap: 10px;
+    align-items: center;
+
+    margin-top: 10px;
+  `,
+  SellerIcon: styled.div`
+    background-image: url("/icons/person.svg");
+    background-repeat: no-repeat;
+    width: 16px;
+    height: 16px;
+  `,
+  DeliveryIcon: styled.div`
+    background-image: url("/icons/delivery.svg");
+    background-repeat: no-repeat;
+    width: 22px;
+    height: 16px;
+  `,
+  greyText: styled.span`
+    font-size: ${(props) => props.theme.font.small};
+    color: ${(props) => props.theme.color.deepGrey};
+  `,
+
+  BtnWrap: styled.div`
+    display: flex;
+    justify-content: space-between;
+
+    width: 100%;
+  `,
 };
--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -1,29 +1,68 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Button from "../../components/Button/Button";
 import { S } from "./Detail.style";
+import { ItemProps } from "../../types/types";
+import Nav from "../../components/Nav/Nav";
 
 const Detail = () => {
+  const [item, setItem] = useState<ItemProps | null>(null);
+
+  const getItem = async () => {
+    try {
+      const res = await axios.get("/data/items.json");
+      setItem(res.data[0]);
+    } catch (err) {
+      console.error("Error fetching item: ", err);
+    }
+  };
+
+  useEffect(() => {
+    getItem();
+  }, []);
+
+  const addToCart = () => {};
+
+  const purchase = () => {};
+
   return (
     <S.DetailWrap>
-      <S.UpperWrap>
-        <S.ProductImg />
-        <S.ProductInfo>
-          <S.ProductName></S.ProductName>
-          <S.ProductPrice></S.ProductPrice>
-          <S.SellerBox>
-            <S.SellerIcon />
-            <S.SellerNick></S.SellerNick>
-          </S.SellerBox>
-          <S.DeliveryBox>
-            <S.DeliveryIcon />
-            <S.DeliveryPrice></S.DeliveryPrice>
-          </S.DeliveryBox>
+      <Nav />
+      <S.Detail>
+        <S.UpperWrap>
+          <S.ProductImg imgUrl={item?.imgUrl} />
+          <S.ProductInfo>
+            <div>
+              <S.EditBtn />
+              <S.ProductName>{item?.name}</S.ProductName>
+              <S.ProductPrice>
+                <S.Bold>{item?.price}</S.Bold> 원
+              </S.ProductPrice>
+              <S.InfoBox>
+                <S.SellerIcon />
+                <S.greyText>랄랄라</S.greyText>
+              </S.InfoBox>
+              <S.InfoBox>
+                <S.DeliveryIcon />
+                <S.greyText>배송비 무료</S.greyText>
+              </S.InfoBox>
+            </div>
 
-          <S.BtnWrap>
-            <Button></Button>
-            <Button></Button>
-          </S.BtnWrap>
-        </S.ProductInfo>
-      </S.UpperWrap>
+            <S.BtnWrap>
+              <Button
+                btnText="장바구니 담기"
+                bgcolor="blue70"
+                handleClick={addToCart}
+              />
+              <Button
+                btnText="바로구매 하기"
+                bgcolor="orange70"
+                handleClick={purchase}
+              />
+            </S.BtnWrap>
+          </S.ProductInfo>
+        </S.UpperWrap>
+      </S.Detail>
     </S.DetailWrap>
   );
 };
