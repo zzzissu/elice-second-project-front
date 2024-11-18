c2051c9d9c352a0280007625909dbf89b8c15cdd
bella <103259907+WolgyeBella@users.noreply.github.com>
2024-11-18 15:14:19 +0900

    🩹 fix: 마이페이지 회원정보수정 프로필 사진 ui 추가

--- src/pages/UserDataEdit/UserDataEdit.styled.ts
+++ src/pages/UserDataEdit/UserDataEdit.styled.ts
@@ -30,34 +30,56 @@ export const InputContainer = styled.div`
 `;
 
 export const ProfilePicture = styled.div`
-  width: 80px;
-  height: 80px;
-  border-radius: 50%;
-  overflow: hidden;
-  background-color: #f0f0f0;
-  margin-right: 20px;
+  width: 100%;
+  height: 100%;
+  text-align: center;
 `;
 
 export const ProfileImage = styled.img`
-  width: 100%;
-  height: 100%;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  overflow: hidden;
+  background-color: #f0f0f0;
   object-fit: cover;
 `;
 
-
 export const FileInputLabel = styled.label`
   display: inline-block;
-  padding: 10px 20px;
-  background-color: #5dade2;
+  width: 50%;
+  padding: 9px 12px 10px;
+  background-color: ${({ theme }) => theme.color.orange70};
   color: white;
   border-radius: 4px;
   text-align: center;
   cursor: pointer;
-  font-size: 14px;
+  font-size: ${({ theme }) => theme.font.small};
+  transition: all 0.2s;
+  &:hover {
+    background-color: ${({ theme }) => theme.color.orange};
+  }
 `;
 
 export const FileInput = styled.input`
   display: none;
+`;
+
+export const FileButton = styled.button`
+  margin-left: 8px;
+  padding: 11px 12px;
+  font-size: 14px;
+  color: #fff;
+  background-color: ${({ theme }) => theme.color.grey};
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: all 0.3s;
+  min-width: 80px;
+  width: 50%;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.color.blue};
+  }
 `;
 
 export const CheckButton = styled.button`
